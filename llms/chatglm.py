b77d7c30d73b44a3f93a27d7707298f86acdc7a4
Jerry-Kon <92307128+Jerry-Kon@users.noreply.github.com>
2023-09-07 14:29:22 +0800

    Update chatglm.py

--- llms/chatglm.py
+++ llms/chatglm.py
@@ -1,6 +1,5 @@
 from transformers import AutoTokenizer, AutoModel
 
-
 class ChatglmChat:
     def __init__(self, model_name_or_path, task=None):
         self.tokenizer = AutoTokenizer.from_pretrained(
